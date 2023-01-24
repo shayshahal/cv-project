2632a58b683a265fdcd71ce872daa60063399ede
shayshahal <shay840@gmail.com>
2023-01-24 19:12:00 +0200

    setup

---
+++ src/components/Practical.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Practical extends Component{
+    render(){
+        return (<div>
+            
+        </div>);
+    };
+}
+
+export default Practical
