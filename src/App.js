9bb187bdb2708aecd2e5393bdca06acaa87891a8
shayshahal <shay840@gmail.com>
2023-01-24 19:12:08 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './styles/App.css';
+import Personal from './components/Personal';
 
 function App() {
   return (
     <div className="App">
-
+      <Personal/>
     </div>
   );
 }
