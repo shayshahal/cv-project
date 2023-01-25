4e0f9f4d06508db73d57742ca77d594e55135c24
shayshahal <shay840@gmail.com>
2023-01-25 20:58:06 +0200

    random fixes

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import '../styles/Input.css'
 
 class Input extends Component{
     constructor(props){
-        super();
+        super(props);
         this.handleChange = this.handleChange.bind(this)
     }
     handleChange(e){
@@ -10,7 +11,7 @@ class Input extends Component{
     }
     render(){
         const {name, type, value} = this.props;
-        return (<label htmlFor={name}>{name + ' '}<input type={type} name={name} value={value} onChange={this.handleChange}/></label>);
+        return (<label htmlFor={name}>{name + ': '}<input type={type} name={name} value={value} onChange={this.handleChange}/></label>);
     };
 }
 
