705748cc2b390a17729d95b8bc7dc9342bfc4a19
shayshahal <shay840@gmail.com>
2023-01-28 20:41:17 +0200

    changed Personal component from class to function

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,19 +1,14 @@
-import React, { Component} from "react";
+import React from "react";
 import Section from "./Section";
 import '../styles/Personal.css'
-class Personal extends Component{
-    render(){
-        return (<Section ind={-1} class='Personal' fields={[
-                {name: 'Full Name', type: 'text', value: 'Full Name'},
-                {name: 'Age', type: 'number', value:''},
-                {name: 'Email',type: 'email', value: ''},
-                {name: 'Phone',type: 'tel', value: ''},
-                {name: 'Address',type: 'text', value: ''},
-                {name: 'About Me',type: 'text', value: ''},
-            ]
-        }/>
-);
-    }
+export default function Personal(){
+    return (<Section ind={-1} class='Personal' fields={[
+            {name: 'Full Name', type: 'text', value: 'Full Name'},
+            {name: 'Age', type: 'number', value:''},
+            {name: 'Email',type: 'email', value: ''},
+            {name: 'Phone',type: 'tel', value: ''},
+            {name: 'Address',type: 'text', value: ''},
+            {name: 'About Me',type: 'text', value: ''},
+        ]
+    }/>);
 }
-
-export default Personal;
