3ec6aafbcd20526cac889d6eb888cb467185ab1f
shayshahal <shay840@gmail.com>
2023-01-28 20:39:57 +0200

    changed Education component from class to function

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,23 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import Section from "./Section";
 import '../styles/Education.css'
-class Education extends Component{
-    constructor(props){
-        super(props);
-        this.onDelete = this.onDelete.bind(this);
+export default function Education({onDelete, ind}){
+    function onRemove(){
+        onDelete(ind, Education)
     }
-    onDelete(){
-        this.props.onDelete(this.props.ind, Education)
-    }
-    render(){
-        return (<Section onDelete={this.onDelete} ind={this.props.ind} class='Education' fields={[
-            {name: 'School/University name', type: 'text', value: 'School/University name'},
-            {name: 'Start',type: 'date', value: ''},
-            {name: 'Finish',type: 'date', value: ''},
-            {name: 'Degree',type: 'text', value: ''},
-            {name: 'Subject',type: 'text', value: ''},
-        ]}/>);
-    };
+    return (<Section onDelete={onRemove} ind={ind} class='Education' fields={[
+        {name: 'School/University name', type: 'text', value: 'School/University name'},
+        {name: 'Start',type: 'date', value: ''},
+        {name: 'Finish',type: 'date', value: ''},
+        {name: 'Degree',type: 'text', value: ''},
+        {name: 'Subject',type: 'text', value: ''},
+    ]}/>);
 }
-
-export default Education
